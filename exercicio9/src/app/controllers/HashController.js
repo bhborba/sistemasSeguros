4c4ac9c4038da95b146221ce994517c8caa2c488
Bruno Henrique de Borba <bhborba@Brunos-Air.teobaldo.local>
2019-11-08 22:33:15 -0300

    Finalizado exercício

--- exercicio9/src/app/controllers/HashController.js
+++ exercicio9/src/app/controllers/HashController.js
@@ -1,6 +1,25 @@
+import Forge from 'node-forge';
+
+const fs = require('fs');
+
 class HashController {
+  // eslint-disable-next-line class-methods-use-this
   index(req, res) {
-    return res.status(200).json({ ops: 'Deu Boa' });
+    const text = fs.readFileSync(req.file.path, 'utf8');
+    let md;
+    if (req.params.tipo === 'sha1') {
+      md = Forge.md.sha1.create();
+      console.log('Fez sha1');
+    } else if (req.params.tipo === 'sha256') {
+      md = Forge.md.sha256.create();
+      console.log('Fez sha256');
+    } else {
+      md = Forge.md.md5.create();
+      console.log('Fez MD5');
+    }
+    md.update(text);
+    console.log(md.digest().toHex());
+    return res.status(200).json({ ok: 'Sucesso' });
   }
 }
 
