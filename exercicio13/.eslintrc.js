5820c3bb6be177d646bc452ef9b4be4621b20efb
bhborba <brunohenriqueborba@gmail.com>
2019-11-27 23:31:32 -0300

    Feito controller, model e database

--- exercicio13/.eslintrc.js
+++ exercicio13/.eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
   env: {
     es6: true,
+    node: true,
   },
   extends: [
     'airbnb-base',
+    'prettier',
   ],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -14,5 +17,10 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+     "camelcase": "off",
+     "no-unused-vars": ["error", {"argsIgnorePattern":"next" }]
   },
 };
